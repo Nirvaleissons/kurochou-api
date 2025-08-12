8660706915292e4a12f774b937583493a41cc26f
meirochun <meirochun@gmail.com>
2025-08-11 23:37:37 -0300

    feat: add environment variables

--- Kurochou.API/Program.cs
+++ Kurochou.API/Program.cs
@@ -2,6 +2,11 @@ using Kurochou.DI;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+        .AddJsonFile("appsettings.json", false, true)
+        .AddJsonFile("appsettings.{builder.Environment.EnvironmentName}.json", true)
+        .AddEnvironmentVariables();
+
 builder.Services.AddDependencies(builder.Configuration);
 
 var app = builder.Build();
