7772c01bc8beed1c6069a765efdbfd7e0c8cf639
MrFelopes <feliperfragoso@gmail.com>
2025-08-11 23:39:41 -0300

    added some of the upload logic

---
+++ Kurochou.Domain/Service/UploadService.cs
@@ -0,0 +1,23 @@
+﻿using Kurochou.Domain.DTO.Clips;
+using Kurochou.Domain.Interface.Service;
+
+namespace Kurochou.Domain.Service;
+
+public class UploadService : IUploadService
+{
+        public async Task<string> Upload(UploadRequest request, CancellationToken cancellationToken)
+        {
+                var sanitizedTitle = string.Concat(request.Title.Split(Path.GetInvalidFileNameChars()));
+                var fileName = $"{sanitizedTitle}{Path.GetExtension(request.File.FileName)}";
+                var savePath = Path.Combine("Uploads", fileName);
+
+                Directory.CreateDirectory("Uploads");
+
+                using (var stream = new FileStream(savePath, FileMode.Create))
+                {
+                        await request.File.CopyToAsync(stream, cancellationToken);
+                }
+
+                return fileName;
+        }
+}
